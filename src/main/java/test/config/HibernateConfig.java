f30933e8e286370c1ce479e083231591e50bc0ab
Feich <maksimov_timofei@mail.ru>
2021-01-09 20:47:55 +0300

    added EntityManager

--- src/main/java/test/config/HibernateConfig.java
+++ src/main/java/test/config/HibernateConfig.java
@@ -9,12 +9,15 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -46,71 +49,38 @@ public class HibernateConfig {
 
     @Bean
     public DataSource dataSource() {
-        BasicDataSource dataSource = new BasicDataSource();
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName(environment.getRequiredProperty("jdbc.driverClassName"));
         dataSource.setUrl(environment.getRequiredProperty("jdbc.url"));
         dataSource.setUsername(environment.getRequiredProperty("jdbc.username"));
         dataSource.setPassword(environment.getRequiredProperty("jdbc.password"));
         return dataSource;
     }
-    //Было
-/*
-    @Bean
-    public LocalSessionFactoryBean sessionFactory() {
-        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-        sessionFactory.setDataSource(dataSource());
-        sessionFactory.setPackagesToScan("test.model");
-        sessionFactory.setHibernateProperties(hibernateProperties());
-        return sessionFactory;
-    }
-    @Bean
-    public HibernateTransactionManager transactionManager() {
-        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
-        transactionManager.setSessionFactory(sessionFactory().getObject());
-        return transactionManager;
-    }*/
-//попробовал подумать
-    @Bean
-    public EntityManagerFactory entityManagerFactory(){
-        LocalContainerEntityManagerFactoryBean containerEntityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
-        containerEntityManagerFactoryBean.setDataSource(dataSource());
-        HibernateJpaVendorAdapter adaptor = new HibernateJpaVendorAdapter();
-        containerEntityManagerFactoryBean.setJpaVendorAdapter(adaptor);
-        containerEntityManagerFactoryBean.setPackagesToScan("test.model");
-        containerEntityManagerFactoryBean.setJpaProperties(hibernateProperties());
-        return containerEntityManagerFactoryBean.getNativeEntityManagerFactory();
-    }
-
-    @Bean
-    public EntityManager entityManager(EntityManagerFactory entityManagerFactory){
-        return entityManagerFactory.createEntityManager();
-    }
-
-
-
-    //просто скопировал
-/*    @Bean
-    public JpaTransactionManager jpaTransactionManager() {
-        JpaTransactionManager transactionManager = new JpaTransactionManager();
-        transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
-        return transactionManager;
-    }
-
-    private HibernateJpaVendorAdapter vendorAdaptor() {
-        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-        vendorAdapter.setShowSql(true);
-        return vendorAdapter;
-    }
 
     @Bean
     public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean em
+                = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(dataSource());
+        em.setPackagesToScan("test.model");
 
-        LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
-        entityManagerFactoryBean.setJpaVendorAdapter(vendorAdaptor());
-        entityManagerFactoryBean.setDataSource(dataSource());
-        entityManagerFactoryBean.setPersistenceProviderClass(HibernatePersistenceProvider.class);
-        entityManagerFactoryBean.setPackagesToScan("test.model");
-        entityManagerFactoryBean.setJpaProperties(hibernateProperties());
-        return entityManagerFactoryBean;
-    }*/
+        JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        em.setJpaVendorAdapter(vendorAdapter);
+        em.setJpaProperties(hibernateProperties());
+
+        return em;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
+
+        return transactionManager;
+    }
+
+    @Bean
+    public PersistenceExceptionTranslationPostProcessor exceptionTranslation(){
+        return new PersistenceExceptionTranslationPostProcessor();
+    }
 }
--- src/main/java/test/dao/CargoDao.java
+++ src/main/java/test/dao/CargoDao.java
@@ -1,59 +1,38 @@
 package test.dao;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
-import test.model.Cargo;
 import org.springframework.stereotype.Repository;
+import test.model.Cargo;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceContext;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.atomic.AtomicLong;
+
 @Repository
 @ComponentScan(basePackages = "test")
 public class CargoDao {
+
     @PersistenceContext
     private EntityManager entityManager;
 
-    /*@Autowired
-    public void setEntityManagerFactory(EntityManager entityManager) {
-        this.entityManager = entityManager;
-    }*/
-
     @SuppressWarnings("unchecked")
     public List<Cargo> allCargoes() {
-        entityManager.getTransaction().begin();
-        List<Cargo> cargoes = entityManager.createQuery("from Cargo").getResultList();
-//        entityManager.close();
-        return cargoes;
+        return entityManager.createQuery("from Cargo").getResultList();
     }
 
     public void add(Cargo cargo) {
-        entityManager.getTransaction().begin();
         entityManager.persist(cargo);
-        entityManager.getTransaction().commit();
     }
 
     public void delete(Cargo cargo) {
-        entityManager.getTransaction().begin();
-        entityManager.remove(cargo);
-        entityManager.getTransaction().commit();
+        entityManager.remove(entityManager.contains(cargo) ? cargo : entityManager.merge(cargo));
     }
 
     public void edit(Cargo cargo) {
-        entityManager.getTransaction().begin();
         entityManager.merge(cargo);
-        entityManager.getTransaction().commit();
     }
 
     public Cargo getById(Long id) {
-        entityManager.getTransaction().begin();
         return entityManager.find(Cargo.class, id);
     }
 }
